7f6022196300cb78380ada7ee8a8075b6e2c39c6
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2024-10-02 18:45:51 -0500

    Delete GestionPedidoAPI directory

--- GestionPedidoAPI/GestionPedidoAPI/Models/Productos.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-public class Producto
-{
-    public int CodigoProducto { get; set; }
-    public string DescripcionProducto { get; set; }
-    public float PrecioUnitario { get; set; }
-    public string Moneda { get; set; }
-}
