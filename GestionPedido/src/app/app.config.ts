4560fc0cf794f2a5d986d9a9495b5d5fd137f6c9
yeshuaaguilar <81573986+yeshua-aguilar@users.noreply.github.com>
2024-10-04 15:51:19 -0500

    instalacion de pimeng y tailwind

--- GestionPedido/src/app/app.config.ts
+++ GestionPedido/src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from "@angular/platform-browser/animations"
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideAnimations()]
 };
